4f1ad24eeeca0395eea362c2b3f8037f651d33d3
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-01-17 23:22:31 -0300

    feat: Estilizar o componente SecaExperieciaTrabalho

--- src/componentes/SecaoExperienciaTrabalho/index.jsx
+++ src/componentes/SecaoExperienciaTrabalho/index.jsx
@@ -2,17 +2,17 @@ import "./style.css";
 
 const SecaoExperienciaTrabalho = () => {
     return (
-        <section>
-            <div> 
-            <h2>Experiências de Trabalho</h2>
+        <section className="secaoExperienciaTrabalho">
+            <div className="secaoExperienciaTrabalhoInfo"> 
+            <h2>Experiências De Trabalho</h2>
             <p>
                 Há mais de 10 anos no mercado de Branding, Design Gráfico, Criação de Sites e Marketing Digital,
                 nos empenhamos diariamente para entregar resultados que tragam impacto aos nossos clientes.
             </p>
             </div>
 
-            <div> 
-                <div>
+            <div className="grupoCards"> 
+                <div className="card">
                     <p>FEVEREIRO 2019- 2021</p>
                     <h3>Digital Consulting</h3>
                     <p>Arasaka Inc.</p>
@@ -20,7 +20,7 @@ const SecaoExperienciaTrabalho = () => {
                     <p>Consultoria em estratégias digitais para todas as etapas do ciclo do projeto, desde a definição inicial até a execução.</p>
                 </div>
 
-                <div>
+                <div className="card">
                     <p>AGOSTO 2016- 2019</p>
                     <h3>Product Designer</h3>
                     <p>E Corp.</p>
@@ -28,7 +28,7 @@ const SecaoExperienciaTrabalho = () => {
                     <p>Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra</p>
                 </div>
 
-                <div>
+                <div className="card">
                     <p>JUNHO 2012- 2016</p>
                     <h3>Web Designer</h3>
                     <p>Pied Piperr StartUp</p>
