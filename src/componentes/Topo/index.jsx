8ed179f147dab1f7600bd47d743bcbca1684a24f
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-01-17 23:21:50 -0300

    feat: Estilizar o componente topo

--- src/componentes/Topo/index.jsx
+++ src/componentes/Topo/index.jsx
@@ -4,9 +4,9 @@ import iconeLua from '../../assets/moon.png';
 
 const Topo = () => {
     return (
-        <header>
-            <img src={logo} alt="logomarca" />
-            <button>
+        <header className="topo">
+            <img className="logo" src={logo} alt="logomarca" />
+            <button className="buttonTopo">
                 <img src={iconeLua} alt="icone"/>
             </button>
         </header>
